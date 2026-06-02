0e64039fe5626cdee38b6820b2781951904714cc
saip_the_king <86388371+saiptheking@users.noreply.github.com>
2026-06-02 14:12:44 -0500

    Change default date type from 'modified' to 'created'

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "saiptheking.github.io/The-Kernel/", 
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
